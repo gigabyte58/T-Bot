06f513b909b9d025768990dc4df33ba585fa9f7d
gigabyte58 <104204078+gigabyte58@users.noreply.github.com>
2024-06-24 01:45:45 +0600

    Update omni.js

--- commands/omni.js
+++ commands/omni.js
@@ -21,7 +21,7 @@ module.exports.run = async function ({ bot, chatId, args }) {
     const config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: `http://flask-app-6b1u.onrender.com/chat?prompt=answer or response me only in english${encodeURIComponent(query)}`,
+        url: `http://flask-app-6b1u.onrender.com/chat?prompt=${encodeURIComponent('answer or response me only in english ' + query)}`,
         headers: {}
     };
 
